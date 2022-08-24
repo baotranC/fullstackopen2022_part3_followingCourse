933758a2c4939ba7dc7b0d13f5234a15ffc119e1
bao <bao-tran.cong-huyen-ton-nu.1@etsmtl.net>
2022-08-23 20:47:21 -0400

    Refactor

--- index.js
+++ index.js
@@ -81,5 +81,6 @@ app.post('/api/notes', (request, response) => {
 })
 
 const PORT = process.env.PORT || 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
