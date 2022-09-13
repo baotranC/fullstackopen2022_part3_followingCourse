1ae53d2bf6cbbceadd9dbee7fc44e26de3ff6147
bao <bao-tran.cong-huyen-ton-nu.1@etsmtl.net>
2022-09-12 20:23:16 -0400

    Add update, delete, error handling

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
-require('dotenv').config()
 const express = require('express')
+const app = express()
 const cors = require('cors')
+require('dotenv').config()
 const Note = require('./models/note')
 
-const app = express()
+const requestLogger = (request, response, next) => {
+    console.log('RL: Method:', request.method)
+    console.log('RL: Path:  ', request.path)
+    console.log('RL: Body:  ', request.body)
+    console.log('RL: ---')
+    next()
+}
+
 app.use(express.json())
-app.use(express.static('build'))
+app.use(requestLogger)
 app.use(cors())
+app.use(express.static('build'))
 
 /// APIs
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
 })
-
 
 app.get('/api/notes', (request, response) => {
     Note.find({}).then(notes => {
@@ -20,18 +28,37 @@ app.get('/api/notes', (request, response) => {
     })
 })
 
-app.get('/api/notes/:id', (request, response) => {
+app.get('/api/notes/:id', (request, response, next) => {
     Note.findById(request.params.id).then(note => {
-        response.json(note)
-    })
+        if (note) {
+            response.json(note)
+        } else {
+            response.status(404).end()
+        }
+    }).catch(error => next(error))
 })
 
-//TODO:
-app.delete('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    notes = notes.filter(note => note.id !== id)
+app.delete('/api/notes/:id', (request, response, next) => {
+    Note.findByIdAndRemove(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
+})
 
-    response.status(204).end()
+app.put('/api/notes/:id', (request, response, next) => {
+    const body = request.body
+
+    const note = {
+        content: body.content,
+        important: body.important,
+    }
+
+    Note.findByIdAndUpdate(request.params.id, note, { new: true })
+        .then(updatedNote => {
+            response.json(updatedNote)
+        })
+        .catch(error => next(error))
 })
 
 app.post('/api/notes', (request, response) => {
@@ -51,6 +78,27 @@ app.post('/api/notes', (request, response) => {
         response.json(savedNote)
     })
 })
+
+
+/// Handler of requests with unknown endpoint
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+app.use(unknownEndpoint)
+
+/// Handler of requests with result to errors
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+// this has to be the last loaded middleware.
+app.use(errorHandler)
+
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
